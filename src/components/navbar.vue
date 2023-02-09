d9bab4fcbda9c256df5690e068a462e893cc4c94
dehwyy <91pony@mail.ru>
2023-02-09 14:38:26 +0300

    fixed navigation slider error

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -5,18 +5,34 @@ import { storeToRefs } from "pinia";
 import { Ref } from "@vue/reactivity";
 
 const {NavbarWrapper, NavItemWrapper} = Widgets.wrappers
-const {useNavbarStore} = GlobalStore.store
 
+const {useNavbarStore} = GlobalStore.store
 const navbarStore = useNavbarStore()
 const {navbarItemsData}: IUseNavbarStore<Ref> = storeToRefs(navbarStore)
+
+const isAccessible = ref(false) as Ref<boolean>
+
+
+const setAccessible = () => {
+  isAccessible.value = !isAccessible.value
+  setTimeout(() => {
+    isAccessible.value = true
+  }, 800)
+}
+
+onMounted(async () => {
+  setTimeout(() => {
+    isAccessible.value = true
+  }, 800)
+})
 
 
 </script>
 
 <template>
-    <NavbarWrapper>
+    <NavbarWrapper :is-accessible="isAccessible">
       <NavItemWrapper v-for="i in navbarItemsData">
-        <NuxtLink :to="i.href" :target="i.blank ? '_blank' :  ''">
+        <NuxtLink :to="i.href" :target="i.blank ? '_blank' :  ''" @click="setAccessible">
           {{i.innerText}}
         </NuxtLink>
         </NavItemWrapper>
--- src/widgets/wrappers/NavbarWrapper.vue
+++ src/widgets/wrappers/NavbarWrapper.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="z-10 bg-white h-24 flex items-center border-b-2 border-b-black fixed top-0 left-0 right-0">
-    <div class="flex gap-16 w-[500px] mx-auto justify-center">
+  <div class="z-10 bg-white h-24 py-5 flex items-center border-b-2 border-b-black fixed top-0 left-0 right-0 relative">
+    <div v-if="!isAccessible" class="w-full h-full bg-pale-green absolute z-50"/>
+    <div class="flex gap-16 w-[500px] mx-auto justify-center relative z-40">
       <slot />
     </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+defineProps<{isAccessible: boolean}>()
+</script>
 
 
